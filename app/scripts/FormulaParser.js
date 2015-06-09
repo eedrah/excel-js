219fea0e9ef9faade0a76ce9ba8e374d4f650a5e
Eedrah <eedrah@users.noreply.github.com>
2015-06-09 22:44:00 +1200

    move FormulaParser into its own file

---
+++ app/scripts/FormulaParser.js
@@ -0,0 +1,23 @@
+﻿/* jshint devel:true */
+'use strict';
+
+function FormulaParser() {
+    this.isValid = false;
+    this.value = null;
+}
+
+FormulaParser.prototype = {
+    parse: function(formula) {
+        this.compute(formula);
+    },
+    compute: function(formula) {
+        try {
+            var value = math.eval(formula);
+            this.isValid = true;
+            this.value = value;
+        } catch (error) {
+            // math.js error - generic error type
+            // keep isValid == false
+        }
+    }
+};
