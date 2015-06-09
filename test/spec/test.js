988eecff1dcfd8e221dd56fd82bba2ef7f8065ab
Eedrah <eedrah@users.noreply.github.com>
2015-06-09 17:09:30 +1200

    test referencehelper numericToAlpha

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,47 @@
-/*global describe, it */
-'use strict';
-
+/* jshint devel:true */
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    'use strict';
 
+    describe('ReferenceHelper', function () {
+        describe('NumericToAlpha', function () {
+            it('should return A when given 1', function () {
+                var actual = ReferenceHelper.NumericToAlpha(1);
+                expect(actual).toBe('A');
+            });
+
+            it('should return B when given 2', function () {
+                var actual = ReferenceHelper.NumericToAlpha(2);
+                expect(actual).toBe('B');
+            });
+
+            it('should return C when given 3', function () {
+                var actual = ReferenceHelper.NumericToAlpha(3);
+                expect(actual).toBe('C');
+            });
+
+            it('should return Z when given 26', function () {
+                var actual = ReferenceHelper.NumericToAlpha(26);
+                expect(actual).toBe('Z');
+            });
+
+            it('should return AA when given 27', function () {
+                var actual = ReferenceHelper.NumericToAlpha(27);
+                expect(actual).toBe('AA');
+            });
+
+            it('should return AB when given 28', function () {
+                var actual = ReferenceHelper.NumericToAlpha(28);
+                expect(actual).toBe('AB');
+            });
+
+            it('should return AZ when given 52', function () {
+                var actual = ReferenceHelper.NumericToAlpha(52);
+                expect(actual).toBe('AZ');
+            });
+
+            it('should return BA when given 53', function () {
+                var actual = ReferenceHelper.NumericToAlpha(53);
+                expect(actual).toBe('BA');
             });
         });
     });
