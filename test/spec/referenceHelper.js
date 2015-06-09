7a4adbf685c159d96b2da003dc21609405498954
Eedrah <eedrah@users.noreply.github.com>
2015-06-10 00:40:34 +1200

    test alphaToNumeric

--- test/spec/referenceHelper.js
+++ test/spec/referenceHelper.js
@@ -44,5 +44,47 @@
                 expect(actual).toBe('BA');
             });
         });
+
+        describe('AlphaToNumeric', function () {
+            it('should return 1 when given A', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('A');
+                expect(actual).toBe(1);
+            });
+
+            it('should return 2 when given B', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('B');
+                expect(actual).toBe(2);
+            });
+
+            it('should return 3 when given C', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('C');
+                expect(actual).toBe(3);
+            });
+
+            it('should return 26 when given Z', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('Z');
+                expect(actual).toBe(26);
+            });
+
+            it('should return 27 when given AA', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('AA');
+                expect(actual).toBe(27);
+            });
+
+            it('should return 28 when given AB', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('AB');
+                expect(actual).toBe(28);
+            });
+
+            it('should return 52 when given AZ', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('AZ');
+                expect(actual).toBe(52);
+            });
+
+            it('should return 53 when given BA', function () {
+                var actual = ReferenceHelper.AlphaToNumeric('BA');
+                expect(actual).toBe(53);
+            });
+        });
     });
 })();
