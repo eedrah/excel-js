0c432ca566581bc280ec88c402a35b071f79227b
Eedrah <eedrah@users.noreply.github.com>
2015-06-09 16:59:56 +1200

    use jasmine

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,12 @@
-/* global describe, it */
+/*global describe, it */
+'use strict';
 
 (function () {
-  'use strict';
+    describe('Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
-      });
+            });
+        });
     });
-  });
 })();
