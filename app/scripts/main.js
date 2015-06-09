17c73a8d9e7113caa5071bc90f371990b6e611c7
Eedrah <eedrah@users.noreply.github.com>
2015-06-09 17:55:57 +1200

    create table in javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,18 @@
 var ReferenceHelper = (function () {
     var letterArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    function NumericToAlpha(number) {
-        if (number === 0){return ""}
+    function numericToAlpha(number) {
+        if (number === 0) {
+            return '';
+        }
         var zeroIndexed = number - 1;
         var lastLetter = letterArray[zeroIndexed % letterArray.length];
         var remainder = Math.floor(zeroIndexed / 26);
-        var otherLetters = NumericToAlpha(remainder);
+        var otherLetters = numericToAlpha(remainder);
         return otherLetters + lastLetter;
     }
 
-    return { NumericToAlpha: NumericToAlpha };
+    return { NumericToAlpha: numericToAlpha };
 })();
 
 function TableBuilder($table) {
@@ -21,7 +23,7 @@ function TableBuilder($table) {
 }
 
 TableBuilder.prototype = {
-    build: function() {
+    build: function () {
         this.addHeaderRow();
         this.addDataRows();
     },
@@ -33,14 +35,26 @@ TableBuilder.prototype = {
         }
         this.$table.append($tr);
     },
-    addDataRows: function() {
-        console.log('data');
-    },
-    addColumnHeaderCell: function($row, rowNumber) {
-        var rowLetter = ReferenceHelper.NumericToAlpha(rowNumber);
+    addColumnHeaderCell: function ($row, columnNumber) {
+        var columnLetter = ReferenceHelper.NumericToAlpha(columnNumber);
         var $th = $('<th>');
-        $th.text(rowLetter);
+        $th.text(columnLetter);
         $row.append($th);
+    },
+    addDataRows: function () {
+        for (var i = 1; i <= 100; i++) {
+            this.addDataRow(i);
+        }
+    },
+    addDataRow: function (rowNumber) {
+        var $tr = $('<tr>');
+        var $th = $('<th>').text(rowNumber);
+        $tr.append($th);
+        for (var i = 1; i <= 100; i++) {
+            var $td = $('<td>').append($('<input type="text">'));
+            $tr.append($td);
+        }
+        this.$table.append($tr);
     }
 };
 
